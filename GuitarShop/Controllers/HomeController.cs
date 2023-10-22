c380cfe58a0cc9f6dc5d8be7aa4060cf0205ed50
scottwesenberg <scowesen@gmail.com>
2023-10-21 23:35:50 -0400

    Add project files.

---
+++ GuitarShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("[action]")]
+        public IActionResult ContactUs()
+        {
+            var contact = new Dictionary<string, string>()
+            {
+                {"Phone", "555-123-4567" },
+                {"Email", "info@mtguitarshop.com" },
+                {"Twitter", "stringstrummin" }
+            };
+            return View(contact);
+        }
+    }
+}
